161f4cdee07cd89bcb02c17c3ecb0a3fc0fb9e7b
POANG1 <patrick@poang.com>
2018-05-15 21:25:22 +0200

    other commit

--- test/lambda.js
+++ test/lambda.js
@@ -1,6 +1,29 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	s3.getObject({
+		'Bucket': "com.slappforge.sigma.test.263248768798.jchamath.us-east-1",
+		'Key': "test"
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+			/*
+			data = {
+				AcceptRanges: "bytes", 
+				ContentLength: 3191, 
+				ContentType: "image/jpeg", 
+				ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
+				LastModified: <Date Representation>, 
+				Metadata: {...}, 
+				TagCount: 2, 
+				VersionId: "null"
+			}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
+	callback(null, 'Successfully executed');
 }
